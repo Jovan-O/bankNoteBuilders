bcef329fdae3348a5e1d36dc552e6168ab4149ee
Sinh <blackwolf7901@gmail.com>
2024-06-15 21:22:23 -0400

    Working on adding views, so far have the index working with a bug I need to Iron out.

--- mysite/coinSocial/views.py
+++ mysite/coinSocial/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import Collection, Item
 
 # Create your views here.
 
+class IndexView(generic.ListView):
+    template_name = "coinSocial/index.html"
+    collection_list = Collection.objects.all()
 
-def index(request):
-    return HttpResponse("welcome to coin social")
+    def get_queryset(self):
+        """Return the user's collections and list them"""
+        return Collection.objects.all()
+
+
+# TODO: Figure out to link all items in a collection to a variable to access them in the views
+class CollectionView(generic.ListView):
+    template_name = "coinSocial/index.html"
+    coin_list = Item.objects.filter(collectionID)
+
+    def get_queryset(self):
+        """Return the user's collections and list them"""
+        return Collection.objects.all()
+
+# TODO: Create more views
