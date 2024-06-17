dd99e4091dc75d50777e12da8f5c019fdfa548d5
Sinh <blackwolf7901@gmail.com>
2024-06-17 19:09:06 -0400

    Login and user authentication now working.

--- mysite/coinSocial/urls.py
+++ mysite/coinSocial/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 app_name = 'coinSocial'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("collection/<int:collectionID>/", views.CollectionView.as_view(), name="collection"),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path("collection/<int:collectionID>/", views.CollectionDetailView.as_view(), name="collection"),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('createaccount/', views.CreateAccountView.as_view(), name='createaccount'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
