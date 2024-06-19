97f90ceb854ecda02d47526bf3e69cda6992ff6d
mynor <mynormend@gmail.com>
2024-06-19 02:32:14 -0400

    Create_collection is being added. Removed Collection_Created.html as it was redundant

--- mysite/coinSocial/urls.py
+++ mysite/coinSocial/urls.py
@@ -8,7 +8,7 @@ app_name = 'coinSocial'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("collection/<int:collectionID>/", views.CollectionDetailView.as_view(), name="collection"),
-    path('create-item/<int:collectionID>/', views.ItemCreateView.as_view(), name='create_post'),
+    path('create-item/<int:collectionID>/', views.ItemCreateView.as_view(), name='create_item'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
     path('register/', views.RegisterView.as_view(), name='register'),
