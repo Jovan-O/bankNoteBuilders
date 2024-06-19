1f1522b01efa616ad566fb185b570c7e05ae0461
Sinh <blackwolf7901@gmail.com>
2024-06-18 22:15:19 -0400

    finished create account view
    created logout button

--- mysite/coinSocial/views.py
+++ mysite/coinSocial/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from .models import Collection, Item
 
 
@@ -51,8 +52,7 @@ class DashboardView(generic.ListView, LoginRequiredMixin):
         return Collection.objects.filter(owner=self.request.user)
 
 
-class CreateAccountView(generic.ListView):
-    template_name = "coinSocial/registration/createaccount.html"
-
-    def get_queryset(self):
-        return HttpResponse("Test")
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('coinSocial:login')
+    template_name = 'registration/register.html'
