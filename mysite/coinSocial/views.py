5b987036a695e1072831c966db2d0bbf406230cd
mynor <mynormend@gmail.com>
2024-06-19 02:02:06 -0400

    All links should work, I have added button to navigate directly to dashboard again

--- mysite/coinSocial/views.py
+++ mysite/coinSocial/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseForbidden
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
 from django.views import generic
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from .models import Collection, Item
-from .forms import CollectionForm
+from .forms import CollectionForm, ItemForm
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 # Create your views here.
 
@@ -30,28 +27,18 @@ class CollectionDetailView(LoginRequiredMixin, generic.TemplateView):
         # Check if the logged-in user is the owner of the collection
         if self.request.user != self.collection.owner:
             return HttpResponseForbidden("You do not own this collection")
-
         return super().dispatch(request, *args, **kwargs)
-# TODO: Figure out to link all items in a collection to a variable to access them in the views
-class CollectionView(generic.ListView):
-    template_name = "coinSocial/collection.html"
-    coin_list = Item.objects.all()
-
-    def get_queryset(self):
-        """Return the user's collections and list them"""
-        return Collection.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['coin_list'] = Item.objects.filter(collection=self.collection)
+        context['item_list'] = Item.objects.filter(collection=self.collection)
         context['collection'] = self.collection
         return context
 
 
-# TODO: Create more views
-
 class PostView(generic.ListView):
     template_name = "coinSocial/post.html"
+
 
 # This used to be a template view
 class DashboardView(generic.ListView, LoginRequiredMixin):
@@ -61,17 +48,10 @@ class DashboardView(generic.ListView, LoginRequiredMixin):
 
     collection_list = Collection.objects.all()
 
-class AccountCreationView(generic.ListView):
-    template_name = "coinSocial/registration/createaccount.html"
-
     def get_queryset(self):
         """Return the user's collections and list them"""
         return Collection.objects.filter(owner=self.request.user)
 
-
-def makecollections(request, collectionID):
-    collection = get_object_or_404(Collection, id=collectionID)
-    items = Item.objects.filter(collection=collection)  # Filter items by collection
 
 class RegisterView(generic.CreateView):
     form_class = UserCreationForm
@@ -79,62 +59,20 @@ class RegisterView(generic.CreateView):
     template_name = 'registration/register.html'
 
 
-class CollectionCreateView(generic.CreateView):
+class CollectionCreateView(SuccessMessageMixin, generic.CreateView):
     model = Collection
     form_class = CollectionForm
     template_name = 'coinSocial/create_collection.html'
     success_url = reverse_lazy('coinSocial:dashboard')
+    success_message = "Collection created successfully!"
 
     def form_valid(self, form):
         form.instance.owner = self.request.user  # Set the owner to the current user
         return super().form_valid(form)
 
-class SearchForm(forms.Form):
-    query = forms.CharField(label='Search', max_length=100)
 
 
-def landing_page(request):
-    form = SearchForm()
-    results = []
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            # Add your search logic here, for example:
-            # results = YourModel.objects.filter(field__icontains=query)
-            # For demonstration, we’ll just echo the query
-            results = [f"Result for '{query}'"]
-    return render(request, 'coinSocial/landing.html', {'form': form, 'results': results})
-
-
-class CollectionForm(forms.ModelForm):
-    class Meta:
-        model = Collection
-        fields = ['nameUG', 'descriptionUG', 'publicUG']
-
-
-def create_collection(request):
-    success = False
-    if request.method == 'POST':
-        form = CollectionForm(request.POST)
-        if form.is_valid():
-            new_collection = form.save(commit=False)
-            new_collection.owner = request.user  # Assign current user as the owner
-            new_collection.save()
-            success = True
-    else:
-        form = CollectionForm()
-    return render(request, 'coinSocial/create_collection.html', {'form': form, 'success': success})
-
-
-class ItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ['collection', 'name', 'value', 'condition', 'origin', 'description', 'dateOfIssue', 'frontImg',
-                  'backImg']
-
-
-def create_item(request):
+def create_post(request):
     success = False
     if request.method == 'POST':
         form = ItemForm(request.POST, request.FILES)
@@ -144,3 +82,25 @@ def create_item(request):
     else:
         form = ItemForm()
     return render(request, 'coinSocial/post.html', {'form': form, 'success': success})
+
+
+class ItemCreateView(generic.CreateView):
+    model = Item
+    form_class = ItemForm
+    template_name = 'coinSocial/post.html'
+    success_url = reverse_lazy('coinSocial:dashboard')
+
+# dispatch will verify that the user is on his own collection
+    def dispatch(self, request, *args, **kwargs):
+        collection_id = self.kwargs.get('collectionID')
+        self.collection = get_object_or_404(Collection, collectionID=collection_id)
+        # Check if the logged-in user is the owner of the collection
+        if self.request.user != self.collection.owner:
+            return HttpResponseForbidden("You do not own this collection")
+
+        return super().dispatch(request, *args, **kwargs)
+# This will create the post
+    def form_valid(self, form):
+        form.instance.collection = self.collection  # Assign the current collection to the item
+        form.instance.owner = self.request.user  # Assign current user as the owner
+        return super().form_valid(form)
